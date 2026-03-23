959f78c2a0d7cfe3cc79f91fe9380ced9db774d4
Miguel <miguelrubioibor@gmail.com>
2026-03-23 20:13:32 +0100

    cambios en diseño de nivel

--- assets/levels/tileset2.tsx
+++ assets/levels/tileset2.tsx
@@ -1,4 +1,139 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset1" tilewidth="16" tileheight="16" spacing="1" tilecount="1456" columns="52">
- <image source="../../../../Grado Superior San Valero/Tercer Curso Contenido/Acceso a Datos y Programacion Multimedia/Programacion Multimedia/Juego Segunda Evaluacion/AA2 2026/Resources Investigation/Rubios Bros/rubios_Characters_/giana/tileset_gianna.png" width="885" height="476"/>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset2" tilewidth="16" tileheight="16" spacing="1" tilecount="1456" columns="52">
+ <image source="tileset_gianna1.png" width="885" height="476"/>
+ <tile id="988">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="989">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="990">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="991">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="992">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="993">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="994">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="995">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="996">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1040">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1041">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1042">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1043">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1044">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1045">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1046">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1047">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1048">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1092">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1093">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1094">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1095">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1096">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1097">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1098">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1099">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
+ <tile id="1100">
+  <properties>
+   <property name="ground" value=""/>
+  </properties>
+ </tile>
 </tileset>
